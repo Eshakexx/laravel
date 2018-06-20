1fa2f4a384422fb2997ba1ff72a8052cd78b4cda
eshakexx <andrii.hroshev@nure.ua>
2018-06-20 14:17:53 +0300

    com

--- database/migrations/2018_06_19_145624_create_recipes_table.php
+++ database/migrations/2018_06_19_145624_create_recipes_table.php
@@ -19,6 +19,26 @@ class CreateRecipesTable extends Migration
             $table->string('description');
             $table->timestamps();
         });
+        Schema::create('ingridient', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+        Schema::create('dimension', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+        Schema::create('ingredients_for_prescription', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('prescription_id')->unsigned();
+            $table->integer('ingridient_id')->unsigned();
+            $table->integer('dimension_id')->unsigned();
+            $table->foreign('prescription_id')->references('id')->on('prescription');
+            $table->foreign('ingridient_id')->references('id')->on('ingridient');
+            $table->foreign('dimension_id')->references('id')->on('dimension');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -28,6 +48,10 @@ class CreateRecipesTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('ingredients_for_prescription');
         Schema::dropIfExists('prescription');
+        Schema::dropIfExists('ingridient');
+        Schema::dropIfExists('dimension');
+
     }
 }
